bb5ae005f583abd018796a0676eeb694ed8e0fdf
Labnan <khalidmasum@iut-dhaka.edu>
2021-08-02 05:01:54 +0600

    Create Programming Contest Router

---
+++ Fest2021/routes/programmingContest.routes.js
@@ -0,0 +1,13 @@
+const ensureAuthenticated = require("../middlewares/auth.middleware");
+const router= require("express").Router();
+const programmingContestController = require("../controllers/mathOlympiad.controller");
+
+router.get("/view",ensureAuthenticated,programmingContestController.view);
+router.post("/create",ensureAuthenticated,programmingContestController.createTeam);
+router.get("/delete/:id",ensureAuthenticated,programmingContestController.deleteTeam);
+router.get("/edit/:id",ensureAuthenticated,programmingContestController.showEditTeam);
+router.post("/edit/",ensureAuthenticated,programmingContestController.editTeam);
+
+
+module.exports = router;
+
